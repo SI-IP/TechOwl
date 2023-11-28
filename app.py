6396cab11d2f7cba8839324e3296998df6893476
Clara Ferraz <claraferraz@proton.me>
2023-11-27 21:35:04 -0300

    mudancasReuniao

--- app.py
+++ app.py
@@ -10,40 +10,45 @@ app.debug = os.environ.get("FLASK_DEBUG") == "True"
 
 
 def carregarGlossario():
-    with open('dados.json', 'r', encoding='utf-8') as db:
+    # r significa read
+    with open("dados.json", "r", encoding="utf-8") as db:
         conceitos = json.load(db)
     return conceitos
 
 
-def salvarGlossario(dicio):
-    with open('dados.json', 'w', encoding='utf-8') as db:
+def salvarGlossario(dicio):  # salva no arquivo
+    # w significa write
+    with open("dados.json", "w", encoding="utf-8") as db:
         json.dump(dicio, db, ensure_ascii=False, indent=4)
+        # load para ler e dump para enviar
+        # ensure_ascii lê caracteres especiais
 
 
-def carregarTarefas(arquivo):
-    with open(arquivo, 'r', encoding='utf-8') as db:
+def carregarTarefas(arquivo):  # Lê o arquivo
+    # arquivo como parâmetro é usado pois temos três arquivos de tarefas
+    with open(arquivo, "r", encoding="utf-8") as db:
         tarefas = [linha.strip() for linha in db.readlines()]
     return tarefas
 
 
 def salvarTarefas(tarefas, arquivo):
-    with open(arquivo, 'w', encoding='utf-8') as db:
+    with open(arquivo, "w", encoding="utf-8") as db:
         for tarefa in tarefas:
-            db.write(tarefa + '\n')
+            db.write(tarefa + "\n")
+            # \n para quebrar a linha no arquivo txt
 
 
 conceitos = carregarGlossario()
 
-listaDeTarefas = carregarTarefas('tarefas.txt')
+listaDeTarefas = carregarTarefas("tarefas.txt")
 
-prioridades = carregarTarefas('tarefaspriorizadas.txt')
+prioridades = carregarTarefas("tarefaspriorizadas.txt")
 
-selecionadas = carregarTarefas('tarefasfeitas.txt')
+selecionadas = carregarTarefas("tarefasfeitas.txt")
 
 
 @app.route("/")
 def index():
-
     return render_template("index.html")
 
 
@@ -52,13 +57,17 @@ def glossario():
     if request.method == "POST":
         termo = request.form["termo"]
         definicao = request.form["definicao"]
-        termo = termo.capitalize().rstrip()
+        termo = (
+            termo.capitalize().rstrip()
+        )  # para uniformizar os termos no arquivo e tirar o espaço no final
         conceitos[termo] = definicao
         salvarGlossario(conceitos)
         return redirect("/glossario")
     else:
         pesquisa = request.args.get("pesquisar", "").lower()
+        # retorna no formato de dicionário e na pesquisa vão ser todos com letra minuscula
         pesquisa = unidecode(pesquisa)
+        # ignora a acentuação (biblioteca do Python)
         if pesquisa:
             pesquisado = {
                 termo: descricao
@@ -68,13 +77,19 @@ def glossario():
         else:
             pesquisado = conceitos
 
-    return render_template("glossario.html", glossario=sorted(conceitos.items()), pesquisado=pesquisado, pesquisa=pesquisa)
+    return render_template(
+        "glossario.html",
+        glossario=sorted(conceitos.items()),  # ordem alfabética
+        pesquisado=pesquisado,
+        pesquisa=pesquisa,
+    )
 
 
 @app.route("/deletar/<string:termo>", methods=["GET", "POST"])
 def deletarTermo(termo):
     del conceitos[termo]
     salvarGlossario(conceitos)
+    # para atualizar o arquivo
     return redirect("/glossario")
 
 
@@ -89,6 +104,8 @@ def alterarTermo(termo):
         conceitos[termo] = novaDefinicao
     elif novaDefinicao == "":
         conceitos.setdefault(novoTermo, conceitos.pop(termo))
+        # adiciona um novo termo e deleta o antigo
+        # setdefault define como será reconfigurada a chave
     else:
         conceitos.setdefault(novoTermo, conceitos.pop(termo))
         conceitos[novoTermo] = novaDefinicao
@@ -99,30 +116,39 @@ def alterarTermo(termo):
 @app.route("/tarefas", methods=["GET", "POST"])
 def tarefas():
     if request.method == "POST":
-        tarefa = request.form['tarefa']
+        tarefa = request.form["tarefa"]
         if tarefa not in listaDeTarefas:
             listaDeTarefas.append(tarefa)
-            salvarTarefas(listaDeTarefas, 'tarefas.txt')
+            salvarTarefas(listaDeTarefas, "tarefas.txt")
         return redirect("/tarefas")
     else:
-        return render_template("tarefas.html", listaDeTarefas=listaDeTarefas, prioridades=prioridades, selecionadas=selecionadas)
+        return render_template(
+            "tarefas.html",
+            listaDeTarefas=listaDeTarefas,
+            prioridades=prioridades,
+            selecionadas=selecionadas,
+            # retorna para o jinja ler os termos e funções no HTML
+        )
 
 
 @app.route("/priorizar/<int:indice>")
 def priorizar(indice):
     mover = listaDeTarefas.pop(indice)
-    salvarTarefas(listaDeTarefas, 'tarefas.txt')
+    salvarTarefas(listaDeTarefas, "tarefas.txt")
     prioridades.append(mover)
-    salvarTarefas(prioridades, 'tarefaspriorizadas.txt')
+    salvarTarefas(prioridades, "tarefaspriorizadas.txt")
     return redirect("/tarefas")
+
+
+# remove a tarefa de uma lista e coloca na outra
 
 
 @app.route("/retirar-prioridade/<int:indice>")
 def retirarPrioridade(indice):
     mover = prioridades.pop(indice)
     listaDeTarefas.append(mover)
-    salvarTarefas(listaDeTarefas, 'tarefas.txt')
-    salvarTarefas(prioridades, 'tarefaspriorizadas.txt')
+    salvarTarefas(listaDeTarefas, "tarefas.txt")
+    salvarTarefas(prioridades, "tarefaspriorizadas.txt")
     return redirect("/tarefas")
 
 
@@ -133,10 +159,11 @@ def checkbox(indice):
         selecionadas.remove(tarefa)
     else:
         selecionadas.append(tarefa)
-    salvarTarefas(selecionadas, 'tarefasfeitas.txt')
+    salvarTarefas(selecionadas, "tarefasfeitas.txt")
     return redirect("/tarefas")
 
 
+# para tarefas de prioridades
 @app.route("/check-up/<int:indice>")
 def checkboxP(indice):
     tarefa = prioridades[indice]
@@ -144,7 +171,7 @@ def checkboxP(indice):
         selecionadas.remove(tarefa)
     else:
         selecionadas.append(tarefa)
-    salvarTarefas(selecionadas, 'tarefasfeitas.txt')
+    salvarTarefas(selecionadas, "tarefasfeitas.txt")
     return redirect("/tarefas")
 
 
@@ -152,9 +179,10 @@ def checkboxP(indice):
 def deletarTarefa(indice):
     if listaDeTarefas[indice] in selecionadas:
         selecionadas.remove(listaDeTarefas[indice])
-    del listaDeTarefas[indice]
-    salvarTarefas(listaDeTarefas, 'tarefas.txt')
-    salvarTarefas(selecionadas, 'tarefasfeitas.txt')
+        # usamos o remove pois não sabemos o índice na lista de tarefas feitas
+        salvarTarefas(selecionadas, "tarefasfeitas.txt")
+    del listaDeTarefas[indice]  # aqui sabemos o índice
+    salvarTarefas(listaDeTarefas, "tarefas.txt")
     return redirect("/tarefas")
 
 
@@ -162,33 +190,35 @@ def deletarTarefa(indice):
 def deletarTarefaPriorizada(indice):
     if prioridades[indice] in selecionadas:
         selecionadas.remove(prioridades[indice])
+        salvarTarefas(selecionadas, "tarefasfeitas.txt")
     del prioridades[indice]
-    salvarTarefas(prioridades, 'tarefaspriorizadas.txt')
-    salvarTarefas(selecionadas, 'tarefasfeitas.txt')
+    salvarTarefas(prioridades, "tarefaspriorizadas.txt")
     return redirect("/tarefas")
 
 
 @app.route("/alterar-tarefa/<int:indice>", methods=["GET", "POST"])
 def editarTarefa(indice):
     novaTarefa = request.form["novatarefa"]
-    if listaDeTarefas[indice] in selecionadas:
-        selecionadas.remove(listaDeTarefas[indice])
-        selecionadas.append(novaTarefa)
-    listaDeTarefas[indice] = novaTarefa
-    salvarTarefas(listaDeTarefas, 'tarefas.txt')
-    salvarTarefas(selecionadas, 'tarefasfeitas.txt')
+    if novaTarefa != "":
+        if listaDeTarefas[indice] in selecionadas:
+            selecionadas.remove(listaDeTarefas[indice])
+            selecionadas.append(novaTarefa)
+            salvarTarefas(selecionadas, "tarefasfeitas.txt")
+        listaDeTarefas[indice] = novaTarefa
+        salvarTarefas(listaDeTarefas, "tarefas.txt")
     return redirect("/tarefas")
 
 
 @app.route("/alterar-up-tarefa/<int:indice>", methods=["GET", "POST"])
 def editarTarefaPriorizada(indice):
     novaTarefa = request.form["novatarefapriorizada"]
-    if prioridades[indice] in selecionadas:
-        selecionadas.remove(prioridades[indice])
-        selecionadas.append(novaTarefa)
-    prioridades[indice] = novaTarefa
-    salvarTarefas(prioridades, 'tarefaspriorizadas.txt')
-    salvarTarefas(selecionadas, 'tarefasfeitas.txt')
+    if novaTarefa != "":
+        if prioridades[indice] in selecionadas:
+            selecionadas.remove(prioridades[indice])
+            selecionadas.append(novaTarefa)
+            salvarTarefas(selecionadas, "tarefasfeitas.txt")
+        prioridades[indice] = novaTarefa
+        salvarTarefas(prioridades, "tarefaspriorizadas.txt")
     return redirect("/tarefas")
 
 
